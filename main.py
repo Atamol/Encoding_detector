caca186bd181b6a97c22b7170e73260005da1209
Atamol <35617831+Atamol@users.noreply.github.com>
2024-05-31 15:00:59 +0900

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import chardet
 
-def detect_encoding(file_path):
-    with open(file_path, 'rb') as f:
-        result = chardet.detect(f.read())
-    return result['encoding']
-
-def search_bots(file_path):
-    encoding = detect_encoding(file_path)
-    with open(file_path, encoding=encoding) as csvfile:
-        reader = csv.reader(csvfile)
-        rows = [row for row in reader]
-    return rows
+file_path = "SAMPLE.csv"
+with open(file_path, 'rb') as f:
+  c = f.read()
+  result = chardet.detect(c)
